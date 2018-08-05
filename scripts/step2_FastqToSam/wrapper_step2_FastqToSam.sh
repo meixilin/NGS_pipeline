106c15ae8c6fcfc925ae6ac3283cef7b2e95b8f8
meixilin <khmldhuh@gmail.com>
2018-08-05 00:25:20 -0700

    step2 on hpc final.

--- scripts/step2_FastqToSam/wrapper_step2_FastqToSam.sh
+++ scripts/step2_FastqToSam/wrapper_step2_FastqToSam.sh
@@ -3,17 +3,18 @@
 QSUB=/u/systems/UGE8.0.1vm/bin/lx-amd64/qsub
 
 # Uncomment the line below and fill in the name of the individuals.
-individuals_array=(1_ML 2_ML 3_ML 4_ML 5_ML B1_ML B2_ML 1_RM 2_RM 3_RM 4_RM 5_RM 6_RM 7_RM 8_RM 9_RM B1_RM)
+individuals_array=(4_ML 5_ML B1_ML B2_ML 1_RM 2_RM 3_RM 4_RM 5_RM 6_RM 7_RM 8_RM 9_RM B1_RM)
+# individuals_array=(2_ML 3_ML)
 for i in "${individuals_array[@]}"
 do
 
 $QSUB -N X${i}FastqToSam_08042018 \
--l highp,h_rt=05:00:00,h_data=8G \
--m bea \
+-l highp,h_rt=01:00:00,h_data=8G \
 -o /u/home/m/meixilin/hoffman_log/X${i}FastqToSam_08042018.out.txt \
 -e /u/home/m/meixilin/hoffman_log/X${i}FastqToSam_08042018.err.txt \
+-wd /u/flashscratch/m/meixilin/NGS_pipeline/scripts/step2_FastqToSam \
 step2_FastqToSam.sh ${i} /u/project/rwayne/meixilin/RW20180719 ../../analyses/step2_FastqToSam
 
-sleep 120
+sleep 20
 
 done
