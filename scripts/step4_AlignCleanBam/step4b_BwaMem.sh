5712d920604c6b5061bdc1e5488115efcdecbce8
meixilin <khmldhuh@gmail.com>
2018-08-05 01:48:21 -0700

    change 4b

--- scripts/step4_AlignCleanBam/step4b_BwaMem.sh
+++ scripts/step4_AlignCleanBam/step4b_BwaMem.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-REFERENCE=../../download/refs/canFam3.fa
+REFERENCE=../../download/refs/ref_sequence_75.fasta
+
+source /u/home/m/meixilin/miniconda2/bin/activate NGSProcess
 
 Individual=$1
 Dir=$2
 
 bwa mem -M -t 3 -p $REFERENCE ${Dir}/${Individual}/${Individual}_CleanBamToFastq.fastq 2>> ${Dir}/${Individual}/${Individual}_Process_BwaMem.txt > ${Dir}/${Individual}/${Individual}_BwaMem.bam
+
+source /u/home/m/meixilin/miniconda2/bin/deactivate
