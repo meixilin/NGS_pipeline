196f484ce8db770e61a76ef4acc59924ac4876da
meixilin <khmldhuh@gmail.com>
2018-08-04 22:48:40 -0700

    change individual array def

--- scripts/step0_mkdir.sh
+++ scripts/step0_mkdir.sh
@@ -8,7 +8,7 @@ cd analyses
 
 mkdir step1_fastqc step2_FastqToSam step3_MarkAdapters step4_AlignCleanBam step5_MarkDups step6_RemoveBadReads step7_BQSR
 
-individuals_array = (1_ML 2_ML 3_ML 4_ML 5_ML B1_ML B2_ML 1_RM 2_RM 3_RM 4_RM 5_RM 6_RM 7_RM 8_RM 9_RM B1_RM)
+individuals_array=(1_ML 2_ML 3_ML 4_ML 5_ML B1_ML B2_ML 1_RM 2_RM 3_RM 4_RM 5_RM 6_RM 7_RM 8_RM 9_RM B1_RM)
 
 for dir in */; do
 for i in "${individuals_array[@]}"; do
--- scripts/step2_FastqToSam/wrapper_step2_FastqToSam.sh
+++ scripts/step2_FastqToSam/wrapper_step2_FastqToSam.sh
@@ -3,7 +3,7 @@
 QSUB=/u/systems/UGE8.0.1vm/bin/lx-amd64/qsub
 
 # Uncomment the line below and fill in the name of the individuals.
-individuals_array = (1_ML 2_ML 3_ML 4_ML 5_ML B1_ML B2_ML 1_RM 2_RM 3_RM 4_RM 5_RM 6_RM 7_RM 8_RM 9_RM B1_RM)
+individuals_array=(1_ML 2_ML 3_ML 4_ML 5_ML B1_ML B2_ML 1_RM 2_RM 3_RM 4_RM 5_RM 6_RM 7_RM 8_RM 9_RM B1_RM)
 for i in "${individuals_array[@]}"
 do
 
