10bd6c10ca718f5729be80056f1cb28d7372b787
Meixi Lin <meixilin@n9983.(none)>
2018-08-05 01:14:51 -0700

    resubmit step2 wrapper

--- scripts/step2_FastqToSam/wrapper_step2_FastqToSam.sh
+++ scripts/step2_FastqToSam/wrapper_step2_FastqToSam.sh
@@ -3,8 +3,9 @@
 QSUB=/u/systems/UGE8.0.1vm/bin/lx-amd64/qsub
 
 # Uncomment the line below and fill in the name of the individuals.
-individuals_array=(4_ML 5_ML B1_ML B2_ML 1_RM 2_RM 3_RM 4_RM 5_RM 6_RM 7_RM 8_RM 9_RM B1_RM)
-# individuals_array=(2_ML 3_ML)
+# individuals_array=(4_ML 5_ML B1_ML B2_ML 1_RM 2_RM 3_RM 4_RM 5_RM 6_RM 7_RM 8_RM 9_RM B1_RM)
+# hoffman2 does not like to do all the work, resubmit with the files 
+individuals_array=(4_ML 5_ML 9_RM)
 for i in "${individuals_array[@]}"
 do
 
