50ac9f1769a0a55342dc1b176eb729215199c991
meixilin <khmldhuh@gmail.com>
2018-08-04 23:07:01 -0700

    step2 activate conda

--- scripts/step2_FastqToSam/step2_FastqToSam.sh
+++ scripts/step2_FastqToSam/step2_FastqToSam.sh
@@ -4,6 +4,8 @@ Individual=$1
 FastqDir=$2
 OutDir=$3
 
+source activate NGSProcess
+
 picard FastqToSam \
 FASTQ=${FastqDir}/${Individual}_R1_001.fastq.gz \
 FASTQ2=${FastqDir}/${Individual}_R2_001.fastq.gz \
@@ -12,3 +14,5 @@ READ_GROUP_NAME=X${Individual}_1a \
 SAMPLE_NAME=X${Individual} \
 LIBRARY_NAME=Lib1 \
 PLATFORM=illumina \
+
+source deactivate
